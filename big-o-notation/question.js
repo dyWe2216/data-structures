8ee6cfd5aec9f50069595b197257384d565b76c0
dyWe2216 <dnleodud@gmail.com>
2023-01-28 18:11:31 +0900

    add: big o space 퀴ㅈ

--- big-o-notation/question.js
+++ big-o-notation/question.js
@@ -67,3 +67,59 @@ function subtotals(array) {
 }
 
 // --- 0(n^2)
+
+// space complexity
+// 11. 아래 함수에 대한 공간 복잡도를 구하세요.
+function logUpTo(n) {
+    for (var i = 1; i <= n; i++) {
+        console.log(i);
+    }
+}
+
+// --- 0(1)
+
+// 12. 아래 함수에 대한 공간 복잡도를 구하세요.
+function logAtMost10(n) {
+    for (var i = 1; i <= Math.min(n, 10); i++) {
+        console.log(i);
+    }
+}
+
+// --- 0(1)
+
+// 13. 아래 함수에 대한 공간 복잡도를 구하세요.
+function logAtMost10(n) {
+    for (var i = 1; i <= Math.min(n, 10); i++) {
+        console.log(i);
+    }
+}
+
+// --- 0(1)
+
+// 14. 아래 함수에 대한 공간 복잡도를 구하세요.
+function onlyElementsAtEvenIndex(array) {
+    var newArray = Array(Math.ceil(array.length / 2));
+    for (var i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            newArray[i / 2] = array[i];
+        }
+    }
+    return newArray;
+}
+
+// --- 0(n)
+
+// 15. 아래 함수에 대한 공간 복잡도를 구하세요.
+function subtotals(array) {
+    var subtotalArray = Array(array.length);
+    for (var i = 0; i < array.length; i++) {
+        var subtotal = 0;
+        for (var j = 0; j <= i; j++) {
+            subtotal += array[j];
+        }
+        subtotalArray[i] = subtotal;
+    }
+    return subtotalArray;
+}
+
+// --- 0(n)
