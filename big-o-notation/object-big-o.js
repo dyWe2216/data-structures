6fdcefa8df70fc726177d4a94aa398159d5a3c02
dyWe2216 <dnleodud@gmail.com>
2023-01-29 15:37:55 +0900

    add: object big o 학습 및 퀴즈

---
+++ big-o-notation/object-big-o.js
@@ -0,0 +1,20 @@
+/**
+    Big O of Objects
+        * Insertion - o(1)
+        * Removal - o(1)
+        * Searching - o(N)
+        * Access o(1)
+
+    Big O of Object Methods
+        * Object.keys - o(N)
+        * Object.values - o(N)
+        * Object.entries - o(N)
+        * hasOwnProperty - o(1)
+ */ 
+
+let instructor = {
+    firstName: 'Kelly',
+    isInstructor: true,
+    favoriteNumbers: [1, 2, 3, 4]
+}
+
