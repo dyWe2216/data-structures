dfddd72ac3957d066bfc893bf305be4933e61296
dyWe2216 <dnleodud@gmail.com>
2023-01-29 15:50:50 +0900

    add: array big o 학습 및 퀴즈

---
+++ big-o-notation/array-big-o.js
@@ -0,0 +1,23 @@
+/**
+    Big O of Arrays
+        * Insertion - It depends...
+        * Removal - It depends...
+        * Searching - o(N)
+        * access - o(1)
+        
+    Big O of Array Operations
+        * push - o(1)
+        * pop - o(1)
+        * shift - o(N)
+        * unshift - o(N)
+        * concat - o(N)
+        * slice - o(N)
+        * splice - o(N)
+        * sort - o(N * log N)
+        * foreach/map/filter/reduce/etc... - o(N)
+        
+        
+ */
+
+let names = ["Michael", "Melissa", "Andrea"];
+let values = [true, {}, [], 2, "awesome"];
